b00846e2213ff8e8183f256e7b18bbb640d28386
Diabolino <matthieugouar@gmail.com>
2017-08-27 15:55:49 +0200

    update example.conf

--- conf/example.conf.sh
+++ conf/example.conf.sh
@@ -8,4 +8,5 @@ NGNBRCONNECT="10"
 NGFROM="test@test.com"
 PARPERCENT="20"
 NZBOUTPUT="/home/***/****"
+RANDOMGROUP=("alt.binaries.gougouland" "alt.binaries.usenet2day" "alt.binaries.usc" "alt.binaries.amazing" "alt.binaries.welovelori" "alt.binaries.insiderz" "alt.binaries.xylo" "alt.binaries.paxer" "alt.binaries.kleverig" "alt.binaries.ijsklontje")
 
