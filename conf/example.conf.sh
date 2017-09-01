2824f2e38c66d324abc7d47c26b5eb74bcd29601
Diabolino <matthieugouar@gmail.com>
2017-09-01 07:20:41 +0200

    ajout du choix dans la conf d'utiliser parpar ou par2cmdline

--- conf/example.conf.sh
+++ conf/example.conf.sh
@@ -5,9 +5,11 @@ NGUSER="USERNAME"
 NGPASS="PASSWORD"
 NGGROUP="alt.binaries.******"
 NGNBRCONNECT="10"
-NGFROM="test@test.com"
-PARPERCENT="20"
+NGFROM="test@test.com" #laisser vide entrainera le random de celui-ci
+PARPERCENT="20" #Ne pas mettre le %
 NZBOUTPUT="/home/***/****"
 NZBTEMP="/nzbtemp/"
-RANDOMGROUP=("alt.binaries.gougouland" "alt.binaries.usenet2day" "alt.binaries.usc" "alt.binaries.amazing" "alt.binaries.welovelori" "alt.binaries.insiderz" "alt.binaries.xylo" "alt.binaries.paxer" "alt.binaries.kleverig" "alt.binaries.ijsklontje")
+PAR2BIN="parpar" #parpar ou bien par2cmdline
+PAR2MEMORY"16000" #valuer en MB
+RANDOMGROUP=("alt.binaries.gougouland" "alt.binaries.usenet2day" "alt.binaries.usc" "alt.binaries.amazing" "alt.binaries.welovelori" "alt.binaries.insiderz" "alt.binaries.xylo" "alt.binaries.paxer" "alt.binaries.kleverig" "alt.binaries.ijsklontje") #a ne remplir que si vous utilisez la fonction fullrandom
 
