0394596eb78f36e310508a63d14c4d463a326e63
Diabolino <matthieugouar@gmail.com>
2017-10-11 21:19:58 +0200

    Ajout d'une vérification si nyuu tourne déjà)

--- conf/example.conf.sh
+++ conf/example.conf.sh
@@ -12,4 +12,8 @@ NZBTEMP="/nzbtemp/"
 PAR2BIN="parpar" #parpar ou bien par2
 PAR2MEMORY="16000" #valuer en MB
 RANDOMGROUP=("alt.binaries.gougouland" "alt.binaries.usenet2day" "alt.binaries.usc" "alt.binaries.amazing" "alt.binaries.welovelori" "alt.binaries.insiderz" "alt.binaries.xylo" "alt.binaries.paxer" "alt.binaries.kleverig" "alt.binaries.ijsklontje") #a ne remplir que si vous utilisez la fonction fullrandom
+PARPERCENT="10"
+MAXSEGNBR="8192" # Nombre de segments, maxi 32768
+MAXRARNUMBER="99" # Nombre de rar maxi 10, 50, 99 selon les preferences
+NYUUUSER="user"
 
